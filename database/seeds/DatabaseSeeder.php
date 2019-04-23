f60f87d627a03f8a6a36dcb15e0dc0dbe16a2146
demyansson <damian.taker@bk.ru>
2019-04-23 07:55:38 +0300

    Updated database seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
+use Horsefly\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -9,8 +11,22 @@ class DatabaseSeeder extends Seeder
      *
      * @return void
      */
+
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@test.com',
+            'password' => Hash::make('admin'),
+            'is_verified' => true,
+            'role' => 2
+        ]);
+        User::create([
+            'name' => 'User',
+            'email' => 'user@test.com',
+            'password' => Hash::make('secret'),
+            'is_verified' => true,
+            'role' => 1
+        ]);
     }
 }
